6eacab388b0e39c84a6ce0e1aaa471ed05f6d2be
RoniPG <ronigmn@gmail.com>
2026-01-16 10:31:19 +0100

    feat: create main Tkinter window

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+def main():
+    root = tk.Tk()
+    root.title("Editor de Texto")
+    root.geometry("800x600")
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
